0f9a98065b070bb58d8b8fcf5382cf5fa524505c
KevinNevile <kevin.nevileO@gmail.com>
2024-09-28 17:34:36 -0300

    Criação da tela Login

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,6 +3,11 @@ import { RouteRecordRaw } from 'vue-router';
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
+    component: () => import('layouts/LoginLayout.vue'),
+    children: [{ path: '', component: () => import('pages/Login.vue') }],
+  },
+  {
+    path: '/auth',
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
   },
