4e0226580c5a5fb3696b3fb6a33ee31c486b40b2
KevinNevile <kevin.nevileO@gmail.com>
2024-10-12 18:23:57 -0300

    att telas

--- src/pages/CadDetalhesAluno/CadPaciente.vue
+++ src/pages/CadDetalhesAluno/CadPaciente.vue
@@ -17,32 +17,32 @@
             <div class="form-column">
               <q-input class="custom-input" filled label="Nome" v-model="nome"/>
               <q-input class="custom-input" filled label="Curso" v-model="curso"/>
-              <q-input class="custom-input" filled label="Idade" v-model="idade"/>
-              <q-input class="custom-input" filled label="Telefone" v-model="telefone"/>
+              <q-input class="custom-input" filled label="Idade" v-model="idade" mask="##"/>
+              <q-input class="custom-input" filled label="Telefone" v-model="telefone" mask="(##) #####-####"/>
 
               <div class="form-group">
                 <label>Residência</label>
-                <q-option-group class="custom-radio horizontal-group" :options="residenciaOptions" type="radio" inline v-model="residencia" />
-                <q-input v-if="residencia === 'Outro'" class="custom-input" filled label="Outra Residência" v-model="residenciaOutro"/>
+                <q-option-group class="custom-radio horizontal-group" :options="residenciaOptions" type="radio" inline v-model="residencia" @input="handleResidenciaChange"/>
+                <q-input v-if="residencia === 'Outro'" class="custom-input" filled label="Outra Residência" v-model="outraCidade" @input="atualizarResidenciaRevisao"/>
               </div>
 
               <div class="form-group">
                 <label>Locomoção</label>
-                <q-option-group class="custom-radio horizontal-group" label="Locomoção" :options="locomocaoOptions" type="radio" inline v-model="locomocao" />
-                <q-input v-if="locomocao === 'Outro'" class="custom-input" filled label="Outra Locomoção" v-model="locomocaoOutro"/>
+                <q-option-group class="custom-radio horizontal-group" label="Locomoção" :options="locomocaoOptions" type="radio" inline v-model="locomocao" @input="handleLocomocaoChange"/>
+                <q-input v-if="locomocao === 'Outro'" class="custom-input" filled label="Outra Locomoção" v-model="outraLocomocao" @input="atualizarLocomocaoRevisao"/>
               </div>
             </div>
 
             <div class="form-column">
-              <q-input class="custom-input" filled label="RA" v-model="ra"/>
+              <q-input class="custom-input" filled label="RA" v-model="ra" mask="######-#"/>
               <q-input class="custom-input" filled label="Termo" v-model="termo"/>
               <q-input class="custom-input date-input" filled label="Data de Nascimento" type="date" v-model="dataNascimento"/>
-              <q-input class="custom-input" filled label="Fixo ou Outro" v-model="fixo"/>
+              <q-input class="custom-input" filled label="Fixo ou Outro" v-model="fixo" mask="(##) ####-####)"/>
 
               <div class="form-group">
                 <label>Família Reside</label>
-                <q-option-group class="custom-radio horizontal-group" :options="familiaOptions" type="radio" inline v-model="familia" />
-                <q-input v-if="familia === 'Outro'" class="custom-input" filled label="Outra Localidade da Família" v-model="familiaOutro"/>
+                <q-option-group class="custom-radio horizontal-group" :options="familiaOptions" type="radio" inline v-model="familia"  @input="handleFamiliaResideChange"/>
+                <q-input v-if="familia === 'Outro'" class="custom-input" filled label="Outra Localidade da Família" v-model="outraFamiliaReside" @input="atualizarFamiliaResideRevisao"/>
               </div>
             </div>
 
@@ -71,7 +71,7 @@
         <div class="form-group">
           <div class="form-row" >
             <label>Ensino Medio Concluido em: </label>
-            <q-input class="custom-input" filled v-model="ensinoMedioAno"/>
+            <q-input class="custom-input" filled v-model="ensinoMedioAno" mask="####"/>
 
             <div style="margin-right: 35%;">
               <q-option-group label="Tipo de Escola" :options="escolaOptions" type="radio" v-model="escola" inline />
@@ -82,25 +82,25 @@
         <div class="form-group">
             <label>Formação Anterior</label>
             <div class="form-row2">
-              <q-option-group class="custom-radio" style="margin-bottom: 17px ;"  :options="[{ label: 'Apenas Ensino Médio Completo', value: 'Apenas Ensino Médio Completo' }]" type="radio" v-model="formacaoAnterior" />
+              <q-option-group class="custom-radio" style="margin-bottom: 17px ;"  :options="[{ label: 'Apenas Ensino Médio Completo', value: 'Apenas Ensino Médio Completo' }]" type="radio" v-model="formacaoAnterior" @change="clearCursoAno"/>
             </div>
 
             <div class="form-row2">
-              <q-option-group class="custom-radio" :options="[{ label: 'Superior Incompleto', value: 'Superior Incompleto' }]" type="radio" v-model="formacaoAnterior" style="margin-right: 100px;"/>
-              <q-input class="custom-input" filled label="Curso" style="width: 500px; margin-right: 186px;" v-model="cursoAnterior"/>
-              <q-input class="custom-input" filled label="Ano" type="number" style="width: 100px;" v-model="ano"/>
+              <q-option-group class="custom-radio" :options="[{ label: 'Superior Incompleto', value: 'Superior Incompleto' }]" type="radio" v-model="formacaoAnterior" style="margin-right: 100px;" @change="clearCursoAno"/>
+              <q-input class="custom-input" filled label="Curso" style="width: 500px; margin-right: 186px;" v-model="cursoAnterior" :disabled="!isSuperiorIncompletoSelected"/>
+              <q-input class="custom-input" filled label="Ano" type="number" style="width: 100px;" v-model="ano" mask="####" :disabled="!isSuperiorIncompletoSelected"/>
             </div>
 
             <div class="form-row2">
-              <q-option-group class="custom-radio" :options="[{ label: 'Superior Completo', value: 'Superior Completo' }]" type="radio" v-model="formacaoAnterior" style="margin-right: 110px;"/>
-              <q-input class="custom-input" filled label="Curso" style="width: 500px; margin-right: 186px;" v-model="cursoAnterior"/>
-              <q-input class="custom-input" filled label="Ano" type="number" style="width: 100px;" v-model="ano"/>
+              <q-option-group class="custom-radio" :options="[{ label: 'Superior Completo', value: 'Superior Completo' }]" type="radio" v-model="formacaoAnterior" style="margin-right: 110px;" @change="clearCursoAno"/>
+              <q-input class="custom-input" filled label="Curso" style="width: 500px; margin-right: 186px;" v-model="cursoAnterior" :disabled="!isSuperiorCompletoSelected"/>
+              <q-input class="custom-input" filled label="Ano" type="number" style="width: 100px;" v-model="ano" mask="####" :disabled="!isSuperiorCompletoSelected"/>
             </div>
 
             <div class="form-row2">
-              <q-option-group class="custom-radio" :options="[{ label: 'Outro', value: 'Outro' }]" type="radio" v-model="formacaoAnterior" style="margin-right: 186px;"/>
-              <q-input class="custom-input" filled label="Curso" style="width: 500px; margin-right: 186px;" v-model="cursoAnterior"/>
-              <q-input class="custom-input" filled label="Ano" type="number" style="width: 100px;" v-model="ano"/>
+              <q-option-group class="custom-radio" :options="[{ label: 'Outro', value: 'Outro' }]" type="radio" v-model="formacaoAnterior" style="margin-right: 186px;" @change="clearCursoAno"/>
+              <q-input class="custom-input" filled label="Curso" style="width: 500px; margin-right: 186px;" v-model="cursoAnterior" :disabled="!isOutroSelected"/>
+              <q-input class="custom-input" filled label="Ano" type="number" style="width: 100px;" v-model="ano" mask="####" :disabled="!isOutroSelected"/>
             </div>
           </div>
 
@@ -118,13 +118,13 @@
             <div class="form-row2">
               <q-option-group class="custom-radio" :options="[{ label: 'Estágio Remunerado', value: 'Estágio Remunerado' }]" type="radio" v-model="profissao" style="margin-right: 110px;"/>
               <q-input class="custom-input" filled label="Local" style="width: 500px; margin-right: 186px;" />
-              <q-input class="custom-input" filled label="C/H Semanal" style="width: 200px;" />
+              <q-input class="custom-input" filled label="C/H Semanal" style="width: 200px;" mask="##"/>
             </div>
 
             <div class="form-row2">
               <q-option-group class="custom-radio" :options="[{ label: 'Empregado', value: 'Empregado' }]" type="radio" v-model="profissao" style="margin-right: 166px;"/>
               <q-input class="custom-input" filled label="Cargo" style="width: 500px; margin-right: 186px;" />
-              <q-input class="custom-input" filled label="C/H Semanal" style="width: 200px;" />
+              <q-input class="custom-input" filled label="C/H Semanal" style="width: 200px;" mask="##"/>
             </div>
           </div>
 
@@ -188,37 +188,39 @@
           <div class="form-row">
             <div class="form-column">
               <h4>Identificação</h4>
-              <p>Nome: {{ nome }}</p>
-              <p>Idade: {{ idade }}</p>
-              <p>Sexo: {{ sexo }}</p>
-              <p>Data de Nascimento: {{ dataNascimento }}</p>
-              <p>RA: {{ ra }}</p>
-              <p>Telefone: {{ telefone }}</p>
-              <p>Residência: {{ residencia }}</p>
-              <p>Residência Outro: {{ residenciaOutro }}</p>
-              <p>Locomoção: {{ locomocao }}</p>
-              <p>Família Reside: {{ familia }}</p>
-              <p>Estado Civil: {{ estadoCivil }}</p>
-              <p>Curso: {{ curso }}</p>
-              <p>Termo: {{ termo }}</p>
-              <p>Auxilio: {{ auxilio }}</p>
+              <p><strong> Nome: </strong> {{ nome }}</p>
+              <p><strong>Idade: </strong> {{ idade }}</p>
+              <p><strong>Sexo: </strong> {{ sexo }}</p>
+              <p><strong>Data de Nascimento: </strong> {{ dataNascimento }}</p>
+              <p><strong>RA: </strong> {{ ra }}</p>
+              <p><strong>Telefone: </strong> {{ telefone }}</p>
+              <p><strong>Residência: </strong> {{ residenciaRevisao  }}</p>
+              <p><strong>Locomoção: </strong> {{ locomocaoRevisao  }}</p>
+              <p><strong>Família Reside: </strong> {{ familiaResideRevisao  }}</p>
+              <p><strong>Estado Civil: </strong> {{ estadoCivil }}</p>
+              <p><strong>Curso: </strong> {{ curso }}</p>
+              <p><strong>Termo: </strong> {{ termo }}</p>
+              <p><strong>Auxilio: </strong> {{ auxilio }}</p>
 
             </div>
             <div class="form-column">
               <h4>Formação</h4>
-              <p>Ensino Médio Concluído em: {{ ensinoMedioAno }}</p>
-              <p>Tipo de Escola: {{ escola }}</p>
-              <p>Formação Anterior: {{ formacaoAnterior }}</p>
-              <p>Profissão: {{ profissao }}</p>
+              <p><strong>Ensino Médio Concluído em: </strong> {{ ensinoMedioAno }}</p>
+              <p><strong>Tipo de Escola: </strong> {{ escola }}</p>
+              <p><strong>Formação Anterior: </strong> {{ formacaoAnterior }}</p>
+              <p><strong>Ano: </strong> {{ ano }}</p>
+              <p><strong>Profissão: </strong> {{ profissao }}</p>
+              <p><strong>Cargo: </strong> {{ cargo }}</p>
+              <p><strong>Carga Horaria: </strong> {{  }}</p>
             </div>
 
 
             <div class="form-column">
               <h4>Universidade</h4>
-              <p>Tempo Dedicado aos Estudos: {{ tempoEstudo }}</p>
-              <p>Áreas a Ser Ajudado: {{ areasAjudado }}</p>
-              <p>Destalhes das Áreas Escolhidas: {{ areasASerAjudado }}</p>
-              <p>Como Gostaria de Ser Ajudado: {{ comoGostariaAjudado }}</p>
+              <p><strong>Tempo Dedicado aos Estudos: </strong> {{ tempoEstudo }}</p>
+              <p><strong>Áreas a Ser Ajudado: </strong> {{ areasAjudado }}</p>
+              <p><strong>Destalhes das Áreas Escolhidas: </strong> {{ areasASerAjudado }}</p>
+              <p><strong>Como Gostaria de Ser Ajudado: </strong> {{ comoGostariaAjudado }}</p>
             </div>
           </div>
         </div>
@@ -235,23 +237,6 @@
 export default {
   data() {
     return {
-      // activeTab: 'identificacao',
-      // sexo: null,
-      // auxilio: null,
-      // estadoCivil: null,
-      // residencia: null,
-      // familia: null,
-      // locomocao: null,
-      // formacaoAnterior: null,
-      // profissao: null,
-      // tempoEstudo: null,
-      // areasAjudado: [],
-      // chegouNuap: null,
-
-      // residenciaOutro: '',
-      // familiaOutro: '',
-      // locomocaoOutro: '',
-
       activeTab: 'identificacao',
       // Dados da Identificação
       nome: '',
@@ -259,11 +244,13 @@ export default {
       idade: '',
       telefone: '',
 
-      residencia: null,
-      residenciaOutro: '',
+      residencia: '',
+      outraCidade: '',
+      residenciaRevisao: '',
 
-      locomocao: null,
-      locomocaoOutro: '',
+      locomocao: '',
+      outraLocomocao: '',
+      locomocaoRevisao: '',
 
       ra: '',
       termo: '',
@@ -271,7 +258,8 @@ export default {
       fixo: '',
 
       familia: '',
-      familiaOutro: '',
+      outraFamiliaReside: '',
+      familiaResideRevisao: '',
 
       sexo: '',
       auxilio: '',
@@ -279,11 +267,13 @@ export default {
 
       // Dados da Formação
       ensinoMedioAno: '',
-      escola: null,
+      escola: '',
+
       formacaoAnterior: null,
       cursoAnterior: '',
       ano: '',
-      profissao: null,
+
+      profissao: '',
 
       // Dados da Universidade
       tempoEstudo: null,
@@ -368,7 +358,88 @@ export default {
         { label: 'Setores Unimar', value: 'Setores Unimar' },
       ],
     }
-  }
+  },
+
+  computed: {
+    isSuperiorIncompletoSelected() {
+      console.log('Checking Superior Incompleto:', this.formacaoAnterior);
+      return this.formacaoAnterior === 'Superior Incompleto';
+    },
+    isSuperiorCompletoSelected() {
+      return this.formacaoAnterior === 'Superior Completo';
+    },
+    isOutroSelected() {
+      return this.formacaoAnterior === 'Outro';
+    }
+  },
+
+  methods: {
+    handleResidenciaChange() {
+      if (this.residencia !== 'Outro') {
+        this.outraCidade = '';
+      }
+    },
+
+    handleLocomocaoChange() {
+      if (this.locomocao !== 'Outro') {
+        this.outraLocomocao = '';
+      }
+    },
+
+    handleFamiliaResideChange() {
+      if (this.familiaReside !== 'Outro') {
+        this.outraFamiliaReside = '';
+      }
+    },
+
+
+    atualizarResidenciaRevisao() {
+      this.residenciaRevisao = this.residencia === 'Outro' && this.outraCidade ? this.outraCidade : this.residencia;
+    },
+    atualizarLocomocaoRevisao() {
+      this.locomocaoRevisao = this.locomocao === 'Outro' && this.outraLocomocao ? this.outraLocomocao : this.locomocao;
+    },
+    atualizarFamiliaResideRevisao() {
+      this.familiaResideRevisao = this.familia === 'Outro' && this.outraFamiliaReside ? this.outraFamiliaReside : this.familia;
+    },
+
+    clearCursoAno() {
+      console.log('clearCursoAno called');
+      this.cursoAnterior = '';
+      this.ano = '';
+    }
+  },
+
+  watch: {
+    residencia(newVal) {
+      this.atualizarResidenciaRevisao();
+    },
+    outraCidade(newVal) {
+      if (this.residencia === 'Outro') {
+        this.atualizarResidenciaRevisao();
+      }
+    },
+
+    locomocao(newVal) {
+      this.atualizarLocomocaoRevisao();
+    },
+    outraLocomocao(newVal) {
+      if (this.locomocao === 'Outro') {
+        this.atualizarLocomocaoRevisao();
+      }
+    },
+
+    familia(newVal) {
+      this.atualizarFamiliaResideRevisao();
+    },
+    outraFamiliaReside(newVal) {
+      if (this.familia === 'Outro') {
+        this.atualizarFamiliaResideRevisao();
+      }
+    },
+  },
+
+
 }
 </script>
 
@@ -481,8 +552,12 @@ export default {
 
 p {
   font-size: 20px;
-  color: #555;
+  color: #00505B90;
   text-transform: capitalize;
+}
+
+h4 {
+  color: #00505B;
 }
 
 .q-btn {
